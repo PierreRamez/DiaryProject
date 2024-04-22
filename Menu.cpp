1bfca16b27b8593bcb0d5a532e2efe20c30c51e8
PierreRamez <pierre.ramez10@gmail.com>
2024-04-22 21:38:29 +0200

    created Menu class
    added string & fstream lib. to PasswordManager
    
    waiting to know how to check the validity of the entered password
    
    added fstream lib. to main

---
+++ Menu.cpp
@@ -0,0 +1,18 @@
+#include "Menu.h"
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+void Menu::displayMainMenu() {
+
+}
+
+void Menu::displayRecordList() {
+
+}
+
+void Menu::getUserChoice(string) {
+
+}
